565062e9a1afe45c85feb62b0c15d9235d7c8fdd
chandhug <64053122+chandhug@users.noreply.github.com>
2021-08-20 23:40:43 -0400

    Add files via upload

--- cypress/config.js
+++ cypress/config.js
@@ -7,18 +7,27 @@ const getEnvironmentVariable = (name) => {
     console.warn('No Environment Variable', name)
 }
 
-const config = getEnvironmentVariable('ENVIRONMENT_NAME', 'test');
+const config = getEnvironmentVariable('ENVIRONMENT_NAME');
 
 
 
 const QA_CONFIG = {
-    URL : 'https://qa.google.com/login',
+    URL : 'https://qa.equipbehavioralhealth.com/login',
+
     ADMIN_USER_NAME : 'admin@test.com',
     ADMIN_USER_PSWD : 'letmein',
-    PROVIDER_USER_NAME : 'qa_provider@test.com',
+
+    PROVIDER_USER_NAME : 'qa_provider@equip.health',
     PROVIDER_USER_PASSWORD : 'providerpswd',
-    THERAPIST_USER_NAME : 'qa_therapist@test.com.',
-    THERAPIST_USER_PASSWORD : 'therapistpswd'
+
+    THERAPIST_USER_NAME : 'qa_therapist@equip.health',
+    THERAPIST_USER_PASSWORD : 'therapistpswd',
+
+    PATIENT_USER_NAME : 'qa_therapist@equip.health',
+    PATIENT_USER_PASSWORD : 'therapistpswd',
+
+    CARER_USER_NAME : 'qa_therapist@equip.health',
+    CARER_USER_PASSWORD : 'therapistpswd'
 
 }
 
@@ -26,9 +35,9 @@ const DEV_CONFIG = {
     URL : 'https://dev.equipbehavioralhealth.com/login',
     ADMIN_USER_NAME : 'dev_admin@test.com',
     ADMIN_USER_PSWD : 'letmein',
-    PROVIDER_USER_NAME : 'dev_provider@test.com',
+    PROVIDER_USER_NAME : 'dev_provider@equip.health',
     PROVIDER_USER_PASSWORD : 'providerpswd',
-    THERAPIST_USER_NAME : 'dev_therapist@test.com',
+    THERAPIST_USER_NAME : 'dev_therapist@equip.health',
     THERAPIST_USER_PASSWORD : 'therapistpswd'
     
 }
@@ -58,10 +67,16 @@ const getConfiguration = (envName) => {
 const configurationForEnv = getConfiguration(config);
 
 export const URL = getConfiguration.URL;
-export const ADMIN_USER_NAME = getConfiguration.ADMIN_USER_NAME;
+export const ADMIN_USER_NAME = getConfiguration.ADMIN_USER_NAME
 export const ADMIN_USER_PSWD = getConfiguration.ADMIN_USER_PSWD
 export const PROVIDER_USER_NAME = getConfiguration.PROVIDER_USER_NAME
 export const PROVIDER_USER_PASSWORD = getConfiguration.PROVIDER_USER_PASSWORD
 export const THERAPIST_USER_NAME = getConfiguration.THERAPIST_USER_NAME
 export const THERAPIST_USER_PASSWORD = getConfiguration.THERAPIST_USER_PASSWORD
+export const PATIENT_USER_NAME  = getConfiguration.PATIENT_USER_NAME
+export const PATIENT_USER_PASSWORD  = getConfiguration.PATIENT_USER_PASSWORD
+export const CARER_USER_NAME = getConfiguration.CARER_USER_NAME
+export const CARER_USER_PASSWORD = getConfiguration.CARER_USER_PASSWORD
+
+
 
